d831422edea17d4f418a380f345c04c7e6742553
Vadim Grashchenko <youngwwad@gmail.com>
2022-07-24 00:09:45 +0300

    feature: factory

--- src/socket/commentator.ts
+++ src/socket/commentator.ts
@@ -5,54 +5,81 @@ import _ from "lodash";
 import { msgWhenFinished } from "./texts";
 import { IMember } from "./types";
 
+enum Actions {
+  greetMe = "greet_me",
+  greetOther = "greet_other",
+  beforeGameCounter = "comment_counter",
+  userFinished = "someone_finished",
+  sendWinners = "comment_winners",
+}
 export interface IGreeting {
   toUser: string;
   toOther: string;
 }
-const GREETINGS_COUNT: number = 2;
-const randomGenerate = (max: number, min: number): number =>
-  Math.floor(Math.random() * (max - min + 1) + min);
+interface IdProps {
+  id: string;
+  actionName: Actions;
+  payload: any;
+}
 
-export class Commentator {
+const MAX_GREETINGS_COUNT: number = 2;
+const MIN_GREETINGS_COUNT: number = 0;
+
+class AbstractSender {
   io: Server;
   room: IRoom;
   constructor(io: Server, room: IRoom) {
     this.io = io;
     this.room = room;
   }
+  public broadCastRoom(actionName: Actions, payload: any): void {
+    this.io.sockets.to(this.room.name).emit(actionName, payload);
+  }
+  public emitToId({ id, actionName, payload }: IdProps): void {
+    this.io.to(id).emit(actionName, payload);
+  }
+  public emitInRoom(actionName: Actions, payload: any): void {
+    this.io.sockets.in(this.room.name).emit(actionName, payload);
+  }
+}
+
+export class Commentator extends AbstractSender {
+  constructor(io: Server, room: IRoom) {
+    super(io, room);
+  }
 
   public sendGreeting(id: string, userName: string): void {
     const { toUser, toOther } = TextGenerator.sendGreeting(userName);
-    this.io.to(id).emit("greet_me", toUser);
-    this.io.sockets.in(this.room.name).except(id).emit("greet_other", toOther);
+    super.emitToId({ id, actionName: Actions.greetMe, payload: toUser });
+    super.broadCastRoom(Actions.greetOther, toOther);
   }
 
   public sendMessageBeforeGame(id: string): void {
     const message: string = TextGenerator.messageBeforeGame();
-    this.io.to(id).emit("comment_counter", message);
+    super.emitToId({
+      id,
+      actionName: Actions.beforeGameCounter,
+      payload: message,
+    });
   }
 
   public sendMessageWhenFinished(userName: string): void {
     const message: string = TextGenerator.messageWhenFinished(userName);
-    this.io.sockets.in(this.room.name).emit("someone_finished", message);
+    super.emitInRoom(Actions.userFinished, message);
   }
 
   public sendResultMessage(id: string, winners: IMember[]): void {
     winners.length = winners.length > 3 ? 3 : winners.length;
     const message: string = TextGenerator.sendMessageWhenGameOver(winners);
-    this.io.to(id).emit("comment_winners", message);
+    super.emitToId({ id, actionName: Actions.sendWinners, payload: message });
   }
 }
-
-export const generateGreeting = (username: string, index: number): string => {
-  return greetings[index]?.toOther + username;
-};
-
-const curriedOtherGreeting = _.curry(generateGreeting);
-
 export class TextGenerator {
   static sendGreeting(userName: string): IGreeting {
-    const rndInt: number = randomGenerate(GREETINGS_COUNT, 0);
+    const rndInt: number = randomGenerate(
+      MAX_GREETINGS_COUNT,
+      MIN_GREETINGS_COUNT
+    );
     return {
       ...greetings[rndInt],
       toOther: curriedOtherGreeting(userName)(rndInt),
@@ -63,12 +90,27 @@ export class TextGenerator {
   }
 
   static messageWhenFinished(userName: string): string {
-    return msgWhenFinished
-      .split(" ")
-      .map((item: string) => (item === "name" ? userName : item))
-      .join(" ");
+    return generateFinishedMessage(msgBeforeGame, userName);
   }
   static sendMessageWhenGameOver(winners: IMember[]): string {
-    return msgResults + winners.map((item: IMember) => item.username).join(" ");
+    return curriedGenerateGameOverText(winners)(msgResults);
   }
 }
+
+export const generateGreeting = (username: string, index: number): string => {
+  return greetings[index]?.toOther + username;
+};
+const curriedOtherGreeting = _.curry(generateGreeting);
+const randomGenerate = (max: number, min: number): number => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const generateFinishedMessage = (msg: string, userName: string): string => {
+  return msgWhenFinished
+    .split(" ")
+    .map((item: string) => (item === "name" ? userName : item))
+    .join(" ");
+};
+const generateGameOverText = (winners: IMember[], msg: string): string => {
+  return msg + winners.map((item: IMember) => item.username).join(" ");
+};
+const curriedGenerateGameOverText = _.curry(generateGameOverText);
